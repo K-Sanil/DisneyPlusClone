2f81a9452c714272535769d4d9cb182b8de3c836
K-Sanil <sanil.kandala@gmail.com>
2022-07-18 04:08:37 +0530

    CloneDP

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Carousel element is in HTML, slider element has relative position this means if we change posn of first elemnt it will have impact on other elements
 //If we change margin left property of first slider and it will effect the others
 
-const carousel = document.querySelector('.carousel');
+let carousel = document.querySelector('.carousel');
 
 let sliders = [];
 
@@ -14,8 +14,6 @@ const createSlide = () => {
     }
 
     //Creating DOM element
-
-    
     let slide = document.createElement('div');
     let imgElement = document.createElement('img');
     let content = document.createElement('div');
@@ -23,14 +21,14 @@ const createSlide = () => {
     let h4 = document.createElement('h4');
     let p = document.createElement('p');
 
+    
     //attaching all elements
-    imgElement.appendChild(document.createTextNode(''));
     h1.appendChild(document.createTextNode(movies[slideIndex].name));
     h4.appendChild(document.createTextNode(movies[slideIndex].subTitle));
     p.appendChild(document.createTextNode(movies[slideIndex].desc));
+
     
     //In html structure H1 is above P element so we will append h1 first into content 
-
     content.appendChild(h1);
     content.appendChild(h4);
     content.appendChild(p);
@@ -40,11 +38,13 @@ const createSlide = () => {
 
     carousel.appendChild(slide);
 
+
     //setting up img
     imgElement.src = movies[slideIndex].image;
     slideIndex++;
 
-    // setting up elements className
+
+    // setting up elements className for styling
     slide.className = 'slider';
     content.className = 'slider-content';
     h1.className = 'movie-title';
@@ -53,22 +53,20 @@ const createSlide = () => {
 
     sliders.push(slide);
 
-    //adding sliding effect 
 
-    if(sliders.length){
+    //adding sliding effect 
+    if(1){
 
         sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length -2)}px)`; //this does the margin movement so that it creates sliding effect
     }
 }
 
-for(let i = 0; i < 3; i++){
-
-    createSlide();
-}
+createSlide();
 
 setInterval(() =>{
     createSlide();
-},3000);
+},2500);
+
 
 
 //VIDEO CARDS 
@@ -88,7 +86,66 @@ vidCards.forEach( item => {
         })
 });
 
-// card SLiders
+//CARD SLIDERS AUTOFILL
+
+let cardC = document.querySelector('.card-container');
+
+let deck = [];
+
+let deckIndex = 0;
+
+const createDeck = () =>{
+
+
+    //DOM Elements 
+    let dek = document.createElement('div');
+    let img = document.createElement('img');
+    let cntn = document.createElement('div');
+    let h2 = document.createElement('h2');
+    let cate = document.createElement('h2'); //category 
+    let h6 = document.createElement('h6');
+    let button = document.createElement('button');
+
+    //attaching elements
+    h2.appendChild(document.createTextNode(sCards[deckIndex].name));
+    cate.appendChild(document.createTextNode(sCards[deckIndex].category));
+    h6.appendChild(document.createTextNode(sCards[deckIndex].desc));
+    button.appendChild(document.createTextNode("Add to watchList"));
+
+    //appending to HTML
+    cntn.appendChild(h2);
+    cntn.appendChild(cate);
+    cntn.appendChild(h6);
+    cntn.appendChild(button);
+
+    dek.appendChild(cntn);
+    dek.appendChild(img);
+
+    cardC.appendChild(dek);
+
+    //img setup
+    img.src = sCards[deckIndex].image;
+    deckIndex++;
+
+    //giving class for CSS
+    dek.className = 'card';
+    img.className = 'cardImg';
+    cntn.className = 'cardBody';
+    h2.className = 'name';
+    cate.className = 'category';
+    h6.className = 'desc';
+    button.className = 'watchList';
+
+    deck.push(dek);
+
+}
+
+for(let i=0; i<2; i++){
+
+    createDeck();
+}
+
+// CARD SLIDERS ARROWS
 
 let cardContainer = document.querySelectorAll('.card-container');
 let preBtn = document.querySelectorAll('.preBtn');
---
+++ deckData.js
@@ -0,0 +1,18 @@
+let sCards = [
+
+    {
+        name:'Moon Knight',
+        category: 'Superhero,Action,Marvel',
+        desc: 'Mild-mannered  Grant identity disorder and shares a body with a enemy.',
+        image: 'images/mk.webp'
+    },
+
+    {
+        name:'Shang- Chi',
+        category:'Fantasy',
+        desc:'Shang-Chi, a martial arts master, must confront the past he thought he  ...',
+        image:'images/sc.webp'
+    }
+
+
+]
